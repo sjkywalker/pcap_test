348de05654ac9d201cdc9cab68055618ffa1cba0
sjkywalker <sjkskywalker@gmail.com>
2018-07-26 16:13:40 +0900

    Update: formatted skeleton code

--- main.cpp
+++ main.cpp
@@ -1,35 +1,44 @@
 #include <pcap.h>
 #include <stdio.h>
 
-void usage() {
-  printf("syntax: pcap_test <interface>\n");
-  printf("sample: pcap_test wlan0\n");
+void usage()
+{
+    printf("syntax: pcap_test <interface>\n");
+    printf("sample: pcap_test wlan0\n");
 }
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    usage();
-    return -1;
-  }
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        usage();
+        return -1;
+    }
 
-  char* dev = argv[1];
-  char errbuf[PCAP_ERRBUF_SIZE];
-  pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-  if (handle == NULL) {
-    fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
-    return -1;
-  }
+    char* dev = argv[1];
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    
+    if (handle == NULL)
+    {
+        fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
+        return -1;
+    }
+    
+    printf("Receiving packets...\n\n");
+    
+    while (true)
+    {
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int res = pcap_next_ex(handle, &header, &packet);
+        if (res == 0) continue;
+        if (res == -1 || res == -2) break;
+        printf("%u bytes captured\n", header->caplen);
+    }
 
-  while (true) {
-    struct pcap_pkthdr* header;
-    const u_char* packet;
-    int res = pcap_next_ex(handle, &header, &packet);
-    if (res == 0) continue;
-    if (res == -1 || res == -2) break;
-    printf("%u bytes captured\n", header->caplen);
-  }
-
-  pcap_close(handle);
-  return 0;
+    pcap_close(handle);
+    
+    return 0;
 }
 
