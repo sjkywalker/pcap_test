879c3af740622fb71c3bef21e8847da03fabf446
sjkywalker <sjkskywalker@gmail.com>
2018-07-26 18:29:03 +0900

    Update: print all packet contents

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
 
 void usage()
 {
@@ -7,7 +9,7 @@ void usage()
     printf("sample: pcap_test wlan0\n");
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
@@ -15,30 +17,39 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    char* dev = argv[1];
+    char *dev = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-    
+    pcap_t *handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+
     if (handle == NULL)
     {
         fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
         return -1;
     }
-    
+
     printf("Receiving packets...\n\n");
-    
+
     while (true)
     {
-        struct pcap_pkthdr* header;
-        const u_char* packet;
+        struct pcap_pkthdr *header;
+        const u_char *packet;
         int res = pcap_next_ex(handle, &header, &packet);
         if (res == 0) continue;
         if (res == -1 || res == -2) break;
         printf("%u bytes captured\n", header->caplen);
+        
+        for (int i = 0; i < header->caplen; i++)
+        {
+            printf("%02x ", packet[i]);
+            if (i % 16 == 7) printf(" ");
+            if (i % 16 == 15) printf("\n");
+        }
+    
+        printf("\n\n");
     }
 
     pcap_close(handle);
-    
+
     return 0;
 }
 
